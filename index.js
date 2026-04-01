ca42b20e67d2dd1e3fcefea9694e54237ea5630a
malik30197 <63046749+malik30197@users.noreply.github.com>
2026-04-01 11:12:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+// 🔴 YAHAN CHANGE KARNA HAI
+const CTRADER_ACCESS_TOKEN = "UL90bWeIwXuhhBnGCq3p2bJ9UgxWi5hEJKUz2FDA5gs";
+const ACCOUNT_ID = "46801974";
+
+// ===== TRADE OPEN =====
+app.post("/trade", async (req, res) => {
+  const data = req.body;
+
+  console.log("Signal:", data);
+
+  try {
+    if (data.type === "OPEN") {
+
+      await axios.post(
+        "https://openapi.ctrader.com/tradingaccounts/" + ACCOUNT_ID + "/orders",
+        {
+          symbolName: data.symbol,
+          tradeSide: data.action === "BUY" ? "Buy" : "Sell",
+          volume: data.lot * 100000,
+          stopLoss: data.sl,
+          takeProfit: data.tp
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + CTRADER_ACCESS_TOKEN,
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+      console.log("Trade Opened");
+    }
+
+    res.send("OK");
+
+  } catch (err) {
+    console.log("Error:", err.response?.data || err.message);
+    res.send("Error");
+  }
+});
+
+// SERVER START
+app.listen(3000, () => console.log("Bot running"));
